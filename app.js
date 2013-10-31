d5bd27ea9b06c6911d1d32c717dacc83f4c68b00
Collin Stedman <cstedman@princeton>
2013-10-31 00:05:35 -0400

    Login works!

--- app.js
+++ app.js
@@ -1,40 +1,63 @@
+// Express
 var express = require('express');
 var app = express();
 app.use(express.compress());
 app.use(express.static(__dirname + '/public'));
 
+// Mongoose db
 var db = require('./models/db');
+var connect = db.connect;
 var User = db.User;
 
-/* The following is a test of the passport authentication middleware */
+// Passport user authentication
 var passport = require('passport'),
 LocalStrategy = require('passport-local').Strategy;
 
+// Connect-flash, for passport messages
+var flash = require('connect-flash');
+
+// Passport authentication
+app.configure(function() {
+  app.use(express.cookieParser());
+  app.use(express.bodyParser());
+  app.use(express.cookieSession({ secret: 'SECRET' }));
+  app.use(flash());
+  app.use(passport.initialize());
+  app.use(passport.session());
+});
+passport.serializeUser(function(user, done) {
+  done(null, user._id);
+});
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
 passport.use(new LocalStrategy(
-  function(username, password, done) {
-    User.findOne({ username: username }, function(err, user) {
+ function(username, password, done) {
+    User.findOne({username: username}, function(err, user) {
       if (err) {
         return done(err); 
       }
       if (!user) {
         return done(null, false, { message: 'Incorrect username.' });
       }
-      if (!user.validPassword(password)) {
+      if (user.password != password) {
         return done(null, false, { message: 'Incorrect password.' });
       }
       return done(null, user);
     });
   }
 ));
-      
-app.get('/test', function(req, res) {
-  res.send('filetest');
-});
 
-app.post('/login',
-  passport.authenticate('local', { successRedirect: '/fail',
-                                   failureRedirect: '/',
-                                   failureFlash: false })
-);
+connect.on('error', console.error.bind(console, 'connection error: '));
+connect.once('open', function callback() {
+      
+  app.post('/login',
+    passport.authenticate('local', { successRedirect: '/yay',
+                                    failureRedirect: '/boo',
+                                    failureFlash: true })
+  );
+});
 
 app.listen(3000);
--- models/db.js
+++ models/db.js
@@ -1,12 +1,22 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/product');
+var opts = { user: 'user', pass: 'vanilla' };
+mongoose.connect('mongodb://localhost/product', opts);
 var db = mongoose.connection;
-var Schema = mongoose.Schema;
+exports.connect = db;
 
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function callback() {
+var Schema = mongoose.Schema;
+var userSchema = Schema({
+  username: String,
+  password: String
+});
+var User = mongoose.model('User', userSchema);
+exports.User = User;
+
+/*db.on('error', console.error.bind(console, 'connection error: '));
+exports.callback = db.once('open', function callback() {
   var userSchema = new Schema({username: String, password: String},
                               {collection: 'Users'});
   var User = mongoose.model('User', userSchema);
   exports.User = User;
 });
+*/
